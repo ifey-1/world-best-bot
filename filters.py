96be9451483f430e135c4b35fabe247ba7351fcb
ifey-1 <ifechiagwu283@gmail.com>
2026-09-22 20:56:46 +0100

    Rename is_valid_gem to is_good_gem with new logic

--- filters.py
+++ filters.py
@@ -1,11 +1,2 @@
-def is_valid_gem(token):
-    mcap = token.get('mcap', 0)
-    liq = token.get('liq', 0)
-    vol = token.get('vol', 0)
-    if not (8000 <= mcap <= 25000):
-        return False
-    if liq < 3000:
-        return False
-    if vol < 500:
-        return False
-    return True
+def is_good_gem(token):
+    return token['mcap'] < 500000 and token['liq'] > 3000
